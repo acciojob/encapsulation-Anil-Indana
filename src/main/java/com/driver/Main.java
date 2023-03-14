c701a40d2a3cd99b1c2e3be126d18fa11cdd6bc2
unknown <you@example.com>
2023-03-14 09:14:17 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        // rw.name = "Niklaus";
+        // System.out.println(rw.name);
+        // name has private access in com.driver.RWOnly
+        rw.setter("Niklaus");
+        System.out.println(rw.getter());
+    }
 }
